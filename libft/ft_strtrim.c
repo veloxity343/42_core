07b16bbfdad9b32777d23d73eb49dc1db412abf8
Ryan Cheong <ryan.cheongtl@gmail.com>
2024-02-26 10:44:11 +0800

    modded strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -14,41 +14,43 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t i;
-	size_t j;
-	char *str;
+	char	*trimmed;
+	size_t	start;
+	size_t	end;
+	size_t	len;
 
-	if (s1 != 0 && set != 0)
-	{
-		i = 0;
-		j = ft_strlen(s1);
-		while (s1[i] && ft_strchr(set, s1[i]))
-			i++;
-		while (s1[j - 1] && ft_strchr(set, s1[j - 1]) && j > i)
-			j--;
-		str = (char *)malloc(sizeof(char) * (j - i + 1));
-		if (str)
-			ft_strlcpy(str, &s1[i], j - i + 1);
-	}
-    return (str);
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	len = end - start + 1;
+	trimmed = (char *)malloc(len);
+	if (!trimmed)
+		return (NULL);
+	ft_strlcpy(trimmed, s1 + start, len);
+	return (trimmed);
 }
 
-/*#include <stdio.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[])
+/*int	main(int argc, char *argv[])
 {
-    if (argc == 3)
-    {
-        char *str = argv[1];
-        char *trim = argv[2];
-        char *result = ft_strtrim(str, trim);
-        if (result == NULL)
-        {
-            printf("Trim failed\n");
-            return (1);
-        }
-        printf("%s\n", result);
-        free (result);
-    }
-    return (0);
+	if (argc == 3)
+	{
+		char *str = argv[1];
+		char *trim = argv[2];
+		char *result = ft_strtrim(str, trim);
+		if (result == NULL)
+		{
+			printf("Trim failed\n");
+			return (1);
+		}
+		printf("%s\n", result);
+		free(result);
+	}
+	return (0);
 }*/
