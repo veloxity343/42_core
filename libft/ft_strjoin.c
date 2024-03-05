6768f1e9f38ebb690bde03b6b37c6a1b83dba39d
veloxity343 <156203160+veloxity343@users.noreply.github.com>
2024-03-05 17:02:28 +0800

    Update ft_strjoin.c
    Null protected

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -17,7 +17,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*str;
 	int		i;
 	int		j;
-
+	
+	if (!s1 || !s2)
+		return (NULL);
 	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (str == NULL)
 		return (NULL);
