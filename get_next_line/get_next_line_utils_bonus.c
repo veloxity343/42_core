c59d63f8271b3468ea524089c9f6ef342c834080
Ryan Cheong <156203160+veloxity343@users.noreply.github.com>
2024-04-09 19:27:36 +0800

    Update get_next_line_utils_bonus.c

--- get_next_line/get_next_line_utils_bonus.c
+++ get_next_line/get_next_line_utils_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 void	ft_bzero(void *s, size_t n)
 {
