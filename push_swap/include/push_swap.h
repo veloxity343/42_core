1acd104969e2ca23f54cce5b4819726c4387918a
Ryan Cheong <rcheong@u90z03s07.42kl.edu.my>
2024-04-30 11:45:33 +0800

    added push_swap

---
+++ push_swap/include/push_swap.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcheong <rcheong@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 12:44:29 by rcheong           #+#    #+#             */
+/*   Updated: 2024/04/29 12:52:15 by rcheong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+#endif
