dc9273f68a9931bed2300725920eee6963b4d2ad
Ryan Cheong <rcheong@u81z03s04.42kl.edu.my>
2024-06-29 16:10:24 +0800

    test

--- push_swap/inc/push_swap.h
+++ push_swap/inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: rcheong <rcheong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 12:44:29 by rcheong           #+#    #+#             */
-/*   Updated: 2024/06/23 16:18:57 by rcheong          ###   ########.fr       */
+/*   Updated: 2024/06/29 16:09:34 by rcheong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,11 +48,11 @@ void				error_exit(int nb);
 void				free_array(char ***str);
 void				free_lst(t_node **stack);
 
-// create stack
+// creating stack
 void				build_stack(char **storage, t_node **a);
 
 // sorting index
-int					find_len(t_node *stack);
+int					get_len(t_node *stack);
 void				sort_array(int **tab, int size);
 void				assign_index(int len, t_node **stack, int *sorted_list);
 void				sorting_index(t_node **stack);
@@ -64,43 +64,40 @@ void				rotate(t_node **stack, char *str, int cost);
 void				rev_rotate(t_node **stack, char *str, int cost);
 void				sort_three(t_node **stack);
 
-// midpoint
+// midpoint operation
 void				midpt_sort(t_node **a, t_node **b);
 void				send_to_b(t_node **a, t_node **b, int *total_nodes_to_go);
 int					find_midpt(t_node *stack);
 void				push_to_b(t_node **a, t_node **b, int *i,
 						int *total_nodes_to_go);
 
-// cost
-void				find_cost(t_node **a, t_node **b);
-int					find_cheapest(t_node **b);
+// calculate cost
+void				get_cost(t_node **a, t_node **b);
+int					get_cheapest(t_node **b);
+int					count_forward(t_node *stack, int point);
+int					count_backward(t_node *stack, int point);
 
 // do both
 void				rotate_both(t_node **a, t_node **b, int cost_a, int cost_b);
 void				rev_rotate_both(t_node **a, t_node **b, int cost_a,
 						int cost_b);
 
-// bidirectional moves counter
-int					count_forward_moves(t_node *stack, int point);
-int					count_backward_moves(t_node *stack, int point);
-
-// target_finder
+// get target
 int					get_target_index(t_node **a, t_node **b);
-void				find_min_and_max(int *a_min, int *a_max, t_node *temp);
-void				check_min_and_max_diff(int *min_pos_diff, int *max_diff,
-						t_node **a, t_node **b);
+void				get_min_max(int *a_min, int *a_max, t_node *temp);
+void				check_diff(int *min_pos_diff, int *max_diff, t_node **a,
+						t_node **b);
 
-// order_stack_a
+// re-sort stack a
 void				sort_stack_a(t_node **stack, int point);
 
 // allocate position
 void				assign_pos(t_node **a, t_node **b);
 
-// reorder_both
+// reorder 
 void				reorder_a_b(int cheapest_pos, t_node **a, t_node **b);
 
 // checker
 void				checker(t_node **a, t_node **b);
-// void	val_and_use_ins(t_node **a, t_node **b, char *instructions);
 
 #endif
