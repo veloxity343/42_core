66a9840a09082c99ad4e89d6d43383cd6b9c1783
Ryan Cheong <ryan.cheongtl@gmail.com>
2024-07-10 23:25:01 +0800

    updated minitalk

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -1,6 +1,6 @@
 #include "../inc/minitalk.h"
 
-void	send_bits(pid_t pid, char c)
+/* void	send_bits(pid_t pid, char c)
 {
 	int	bit;
 
@@ -38,4 +38,60 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	return (0);
+} */
+
+volatile sig_atomic_t ack_received = 0;
+
+void ack_handler(int sig)
+{
+    (void)sig;
+    ack_received = 1;
+}
+
+void send_bits(pid_t pid, char c)
+{
+    int bit = 0;
+
+    while (bit < 8)
+    {
+        if ((c & (0x01 << bit)) != 0)
+            kill(pid, SIGUSR1);
+        else
+            kill(pid, SIGUSR2);
+        usleep(100);
+        bit++;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    pid_t pid;
+    char *msg;
+
+    if (argc == 3)
+    {
+        pid = ft_atoi(argv[1]);
+        msg = argv[2];
+
+        signal(SIGUSR1, ack_handler);
+        while (*msg != '\0')
+        {
+            send_bits(pid, *msg);
+            msg++;
+        }
+        send_bits(pid, '\n');
+
+        // Wait for acknowledgment
+        while (!ack_received) {
+            pause();
+        }
+
+        ft_printf("Message received.\n");
+    }
+    else
+    {
+        ft_printf("Usage: %s <PID> <STRING>\n", argv[0]);
+        return (1);
+    }
+    return (0);
 }
--- minitalk/src/server.c
+++ minitalk/src/server.c
@@ -1,6 +1,6 @@
 #include "../inc/minitalk.h"
 
-void	sig_handler(int sig)
+/* void	sig_handler(int sig)
 {
 	static int	bit;
 	static int	i;
@@ -24,7 +24,7 @@ int	main(int argc, char **argv)
 	if (argc != 1)
 		ft_printf("Usage: %s\n", argv[0]);
 	pid = getpid();
-	ft_printf("PID: %d\nWaiting for a message...\n", pid);
+	ft_printf("PID: %d\nWaiting for message...\n", pid);
 	while (argc == 1)
 	{
 		signal(SIGUSR1, sig_handler);
@@ -32,4 +32,58 @@ int	main(int argc, char **argv)
 		pause();
 	}
 	return (0);
+} */
+
+pid_t client_pid = 0;
+
+void sig_handler(int sig, siginfo_t *info, void *context)
+{
+    static int bit = 0;
+    static int i = 0;
+
+    (void)context;
+
+    if (client_pid == 0) {
+        client_pid = info->si_pid;
+    }
+
+    if (sig == SIGUSR1)
+        i |= (0x01 << bit);
+    bit++;
+    if (bit == 8)
+    {
+        ft_printf("%c", i);
+        if (i == '\n') {
+            ft_printf("\nMessage received.\n");
+            kill(client_pid, SIGUSR1);
+            client_pid = 0;
+        }
+        bit = 0;
+        i = 0;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    pid_t pid;
+    struct sigaction sa;
+
+    (void)argv;
+    if (argc != 1) {
+        ft_printf("Usage: %s\n", argv[0]);
+        return (1);
+    }
+    pid = getpid();
+    ft_printf("PID: %d\nWaiting for message...\n", pid);
+
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = sig_handler;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+
+    while (1) {
+        pause();
+    }
+    return (0);
 }
