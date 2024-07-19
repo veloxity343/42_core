745119632a9ac63e2e7a39ba402c9b685281a0fc
Ryan Cheong <rcheong@u90z08s01.42kl.edu.my>
2024-07-19 14:42:36 +0800

    added so_long eg

--- so_long/inc/so_long.h
+++ so_long/inc/so_long.h
@@ -6,7 +6,7 @@
 /*   By: rcheong <rcheong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 10:56:00 by rcheong           #+#    #+#             */
-/*   Updated: 2024/07/19 11:04:08 by rcheong          ###   ########.fr       */
+/*   Updated: 2024/07/19 14:39:03 by rcheong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 # include "../libft/inc/libft.h"
 # include "../libft/inc/get_next_line.h"
 # include "../libft/inc/ft_printf.h"
+# include <fcntl.h>
+# include <mlx.h>
 
 # define IMG_SIZE 64
 
