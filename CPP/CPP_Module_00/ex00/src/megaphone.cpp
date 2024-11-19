698407727374df18a61def652c5d42989b0ce2eb
Ryan Cheong <ryan.cheongtl@gmail.com>
2024-11-19 18:19:47 +0800

    modded make; edited mega

--- CPP/CPP_Module_00/ex00/src/megaphone.cpp
+++ CPP/CPP_Module_00/ex00/src/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: rcheong <rcheong@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 12:05:34 by rcheong           #+#    #+#             */
-/*   Updated: 2024/11/18 12:05:35 by rcheong          ###   ########.fr       */
+/*   Updated: 2024/11/18 16:38:05 by rcheong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,8 @@ int main(int argc, char **argv)
 		for (int i = 1; i < argc; i++)
 		{
 			for (int j = 0; argv[i][j] != '\0'; j++)
-				argv[i][j] = (char)toupper(argv[i][j]);
-			std::cout << argv[i];
-			if (argv[i] != NULL)
-				std::cout << " ";
+				std::cout << (char)toupper(argv[i][j]);
+			std::cout << " ";
 		}
 		std::cout << std::endl;
 	}
