e46649e2d7981ac666f6023af243c42ec0e2ef9d
Ryan Cheong <rcheong@u82z01s06.42kl.edu.my>
2024-11-21 17:28:59 +0800

    updated accounts

--- cpp/module_00/ex02/src/Account.cpp
+++ cpp/module_00/ex02/src/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: rcheong <rcheong@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 11:27:14 by rcheong           #+#    #+#             */
-/*   Updated: 2024/11/21 17:11:59 by rcheong          ###   ########.fr       */
+/*   Updated: 2024/11/21 17:28:12 by rcheong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ int	Account::_totalAmount = 0;
 int	Account::_totalNbDeposits = 0;
 int	Account::_totalNbWithdrawals = 0;
 
+/**
+ * @brief Constructor for a new Account object
+ * @details This constructor will create a new Account object with the given initial deposit amount
+ * @param initial_deposit The initial deposit amount
+ */
 Account::Account( int initial_deposit )
 		:_accountIndex(Account::_nbAccounts),
 		_amount(initial_deposit),
@@ -33,30 +38,55 @@ Account::Account( int initial_deposit )
 	std::cout << "created" << std::endl;
 }
 
-Account::~Account( void ) {
+/**
+ * @brief Destructor for the Account object
+ * @details This destructor will display the timestamp, the account index, the amount, and the message "closed"
+ */
+Account::~Account( void )
+{
 	Account::_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";";
 	std::cout << "amount:" << this->_amount << ";";
 	std::cout << "closed" << std::endl;
 }
 
-int	Account::getNbAccounts( void ) {
+/**
+ * @brief Get the number of accounts
+ */
+int	Account::getNbAccounts( void )
+{
 	return Account::_nbAccounts;
 }
 
-int	Account::getTotalAmount( void ) {
+/**
+ * @brief Get the total amount for all accounts
+ */
+int	Account::getTotalAmount( void )
+{
 	return Account::_totalAmount;
 }
 
-int	Account::getNbDeposits( void ) {
+/**
+ * @brief Get the number of deposits
+ */
+int	Account::getNbDeposits( void )
+{
 	return Account::_totalNbDeposits;
 }
 
-int	Account::getNbWithdrawals( void ) {
+/**
+ * @brief Get the number of withdrawals
+ */
+int	Account::getNbWithdrawals( void )
+{
 	return Account::_totalNbWithdrawals;
 }
 
-void	Account::_displayTimestamp( void ) {
+/**
+ * @brief Display the timestamp in the format [YYYYMMDD_HHMMSS]
+ */
+void	Account::_displayTimestamp( void )
+{
 	time_t		now = time(0);
 	struct tm	*tm = localtime(&now);
 
@@ -71,7 +101,11 @@ void	Account::_displayTimestamp( void ) {
     std::cout << "] ";
 }
 
-void	Account::displayAccountsInfos( void ) {
+/**
+ * @brief Display the account information
+ */
+void	Account::displayAccountsInfos( void )
+{
 	_displayTimestamp();
 	std::cout << "accounts:" << getNbAccounts() << ";";
 	std::cout << "total:" << getTotalAmount() << ";";
@@ -79,7 +113,13 @@ void	Account::displayAccountsInfos( void ) {
 	std::cout << "withdrawals:" << getNbWithdrawals() << std::endl;
 }
 
-void	Account::makeDeposit( int deposit ) {
+/**
+ * @brief Make a deposit into the account
+ * @details This function will increase the amount in the account by the deposit amount
+ * @param deposit The deposit amount
+ */
+void	Account::makeDeposit( int deposit )
+{
 	int	p_amount = this->_amount;
 
 	this->_amount += deposit;
@@ -94,17 +134,26 @@ void	Account::makeDeposit( int deposit ) {
 	std::cout << "nb_deposits:" << this->_nbDeposits << std::endl;
 }
 
-bool	Account::makeWithdrawal( int withdrawal ) {
+/**
+ * @brief Make a withdrawal from the account
+ * @details This function will decrease the amount in the account
+ * by the withdrawal amount unless the account does not have enough funds
+ * @param withdrawal The withdrawal amount
+ */
+bool	Account::makeWithdrawal( int withdrawal )
+{
 	int	p_amount = this->_amount;
 
 	_displayTimestamp();
-	if (checkAmount() < withdrawal) {
+	if (checkAmount() < withdrawal)
+	{
 		std::cout << "index:" << this->_accountIndex << ";";
 		std::cout << "p_amount:" << p_amount << ";";
 		std::cout << "withdrawal:" << "refused" << std::endl;
 		return (false);
 	}
-	else {
+	else
+	{
 		this->_amount -= withdrawal;
 		this->_nbWithdrawals++;
 		Account::_totalAmount -= withdrawal;
@@ -118,11 +167,20 @@ bool	Account::makeWithdrawal( int withdrawal ) {
 	}
 }
 
-int	Account::checkAmount( void ) const {
+/**
+ * @brief Check the amount in the account
+ */
+int	Account::checkAmount( void ) const
+{
 	return this->_amount;
 }
 
-void	Account::displayStatus( void ) const {
+/**
+ * @brief Print the account status summary
+ * @details This function will display the timestamp, the account index, the amount, the number of deposits, and the number of withdrawals
+ */
+void	Account::displayStatus( void ) const
+{
 	_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";";
 	std::cout << "amount:" << this->_amount << ";";
