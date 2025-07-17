4c3349340dbf665b79212d87b7e1f376b43a90f9
Ryan Cheong <ryan.cheongtl@gmail.com>
2025-07-17 23:12:32 +0800

    main function

--- cpp/module_06/ex01/src/main.cpp
+++ cpp/module_06/ex01/src/main.cpp
@@ -1,27 +1,36 @@
-#include "../inc/ScalarConverter.hpp"
-#include "../inc/Data.hpp"
+#include "../inc/Serializer.hpp"
 
 int main() {
-	Data original;
-	original.id = 42;
-	original.score = 98.6f;
-	original.grade = 'A';
+	Data ptr2;
+	ptr2.name = "Michaela Mustermann";
+	ptr2.age = 42;
+	ptr2.next = NULL;
 
-	std::cout << "Original Data:\n";
-	std::cout << "ID: " << original.id << "\n";
-	std::cout << "Score: " << original.score << "\n";
-	std::cout << "Grade: " << original.grade << "\n";
+	Data ptr;
+	ptr.name = "Max Mustermann";
+	ptr.age = 42;
+	ptr.next = &ptr2;
 
-	// Serialize
-	uintptr_t raw = Serializer::serialize(&original);
-	std::cout << "\nSerialized pointer: " << raw << "\n";
+	std::cout << "Here is the original struct:" <<
+		"\n\taddress: " << &ptr <<
+		"\n\tname: " << ptr.name <<
+		"\n\tage: " << ptr.age <<
+		"\n\tnext address: " << ptr.next << "\n";
 
-	// Unserialize
+	std::cout << "\tptr2 address: " << &ptr2 <<
+		"\n\tname: " << ptr2.name <<
+		"\n\tage: " << ptr2.age <<
+		"\n\tnext address: " << ptr2.next << "\n\n";
+
+	// No instance of Serializer needed
+	uintptr_t raw = Serializer::serialize(&ptr);
 	Data* deserialized = Serializer::unserialize(raw);
-	std::cout << "\nDeserialized Data:\n";
-	std::cout << "ID: " << deserialized->id << "\n";
-	std::cout << "Score: " << deserialized->score << "\n";
-	std::cout << "Grade: " << deserialized->grade << "\n";
+
+	std::cout << "Here is the deserialized struct:" <<
+		"\n\taddress: " << deserialized <<
+		"\n\tname: " << deserialized->name <<
+		"\n\tage: " << deserialized->age <<
+		"\n\tnext address: " << deserialized->next << "\n";
 
 	return 0;
 }
